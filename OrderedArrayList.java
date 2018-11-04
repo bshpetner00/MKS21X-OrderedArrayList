1dd34f923a65e47f0a657db31e5acfbb0090170e
Ben Shpetner <bshpetner00@stuy.edu>
2018-11-04 16:43:39 -0500

    finished Ordered

---
+++ OrderedArrayList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+	public OrderedArrayList() {}
+	public OrderedArrayList(int capacity) {
+		super(capacity);
+	}
+	public int getIndex(T item) {
+		for (int i = 0; i < this.size(); i++) {
+			if (this.get(i).compareTo(item) > 0) {
+				return i;
+			}
+		}
+		return index;
+	}
+	public void add(int index, T item) {
+		add(item);
+	}
+	public boolean add(T item) {
+		if (item == null) {
+			throw new IllegalArgumentException("Can't be null!");
+		}
+		int i = this.getIndex(item);
+		super.add(i, item);
+		return true;
+	}
+	public T set(int i, T item) {
+		if (item == null) {
+			throw new IllegalArgumentException("Can't be null!");
+		}
+		T oldItem = remove(i);
+		add(item);
+		return oldItem;
+	}
+}
